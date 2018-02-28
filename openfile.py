af28b8d11ee38d24572126e5fb7c5d82d7e909d2
brensw <sweeney.b@me.com>
2018-02-28 11:39:39 +0000

    Comments added

--- openfile.py
+++ openfile.py
@@ -1,12 +1,12 @@
 #Brendan Sweeney, 28/02/2018
 #Program to display Iris.csv in a pleasing format
 
-import csv
-with open("data\iris.csv",) as f:
-    f_read= csv.reader(f)
+import csv #import csv tools
+with open("data\iris.csv",) as f: # open file
+    f_read= csv.reader(f) #read csv data
     print("Fisher's Iris data set:  ")
-    for row in f_read:
-        print('{:<6}  {:<6}  {:<6}  {:<6}  {:<}'.format(*row))
+    for row in f_read: # For all rows in file
+        print('{:<6}  {:<6}  {:<6}  {:<6}  {:<}'.format(*row)) #display in this format. 
         
 
 
