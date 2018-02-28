87dd0a08d237e1675616a03a121076b551299184
brensw <sweeney.b@me.com>
2018-02-28 11:42:41 +0000

    Comments update

--- openfile.py
+++ openfile.py
@@ -6,7 +6,7 @@ with open("data\iris.csv",) as f: # open file
     f_read= csv.reader(f) #read csv data
     print("Fisher's Iris data set:  ")
     for row in f_read: # For all rows in file
-        print('{:<6}  {:<6}  {:<6}  {:<6}  {:<}'.format(*row)) #display in this format. 
+        print('{:<6}  {:<6}  {:<6}  {:<6}  {:<}'.format(*row)) #Align and space data to be displayed. 
         
 
 
