00ba27426fd2d65e7f5f2f9a61f080921226da0b
brensw <sweeney.b@me.com>
2018-02-28 11:35:14 +0000

    Updated

--- openfile.py
+++ openfile.py
@@ -2,7 +2,7 @@
 #Program to display Iris.csv in a pleasing format
 
 import csv
-with open("data\iris.csv") as f:
+with open("data\iris.csv",) as f:
     f_read= csv.reader(f)
     print("Fisher's Iris data set:  ")
     for row in f_read:
