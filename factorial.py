3899e25c0485f3aeab3676c7c24cda0c3dd6ebdf
brensw <sweeney.b@me.com>
2018-03-06 08:05:20 +0000

    Added 5,7 & 10 factorials, added comments

--- factorial.py
+++ factorial.py
@@ -1,10 +1,16 @@
+#Brendan Sweeney,05/03/2018
+#Factorial Function
+
 def factorial(n_in):
     fact = 1
-    while (n_in > 0):
-        fact = fact * n_in
-        n_in = n_in - 1
-    return fact 
-print(factorial(12))
+    while (n_in > 0):# set limit on number of loops
+        fact = fact * n_in #set fact to value of n_in
+        n_in = n_in - 1 #then reduce n_in by one on each pass
+    return fact # return final value of fact at end of looping
+print("The factorial of 5 is", factorial(5))
+print("The factorial of 7 is", factorial(7))
+print("The factorial of 10 is", factorial(10))
+
 
 
 
