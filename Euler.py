f8f354a3a8325414c44eb8fa3b6cab65ed0acf91
brensw <36295083+brensw@users.noreply.github.com>
2018-04-08 11:39:30 +0100

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -1,7 +1,7 @@
 #Brendan Sweeney. 19/02/2018
 #Euler Project problem_5
 
-# Modified code from stack exchange
+# Modified code from stack exchange https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
 smallest_num = 1 #Create varible
 for i in range (1,21): 
     if smallest_num % i != 0: # Check if smallest_num is not evenly divisable by i then 
