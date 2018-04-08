fce8ad276a01e465a0736413a9a529afe1b2ab74
brensw <36295083+brensw@users.noreply.github.com>
2018-04-08 11:10:07 +0100

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -2,12 +2,12 @@
 #Euler Project problem_5
 
 # Modified code from stack exchange
-smallest_num = 1
+smallest_num = 1 #Create varible
 for i in range (1,21): 
     if smallest_num % i != 0: # Check if smallest_num is not evenly divisable by i then 
         for k in range (1,21):
           
             if (smallest_num * k) % i == 0: # If number evenly divisible by i then   
                 smallest_num = smallest_num * k # Change value of smallest_num
-                break #back to i loop
+                break #back to inital loop
 print ("The smallest number the can be divided evenly by all the numbers between 1 to 20 is" ,smallest_num)
